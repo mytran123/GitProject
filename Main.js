ef95b528bc432c768fda1478d8d41a0eab621cd5
mytran123 <mytran091196@gmai.com>
2021-09-28 09:30:03 +0700

    Adam-Eva

---
+++ Main.js
@@ -0,0 +1,11 @@
+let apple = new Apple(10);
+let human = new Human("Em A", "Male", 50)
+human.say("Ăn táo nào")
+console.log(apple.getWeight())
+console.log(human.getWeight())
+human.eat(apple)
+console.log(apple.getWeight())
+console.log(human.getWeight())
+console.log(human.getName())
+console.log(human.getGender())
+console.log(human.getWeight())
